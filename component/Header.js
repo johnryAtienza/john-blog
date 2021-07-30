c743686ecb33b6c796a10027dc5d89c0d867e249
Johnry Atienza <johnry.atienza@upraxis.com>
2021-07-30 17:31:50 +0800

    First Commit

---
+++ component/Header.js
@@ -0,0 +1,13 @@
+import headerStyle from '../styles/Header.module.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className={headerStyle.title}>
+                Blog Examination
+            </h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ pages/blog.js
@@ -0,0 +1,9 @@
+const blog = () => {
+    return (
+        <div>
+            <h1>This is blog</h1>
+        </div>
+    )
+}
+
+export default blog;
